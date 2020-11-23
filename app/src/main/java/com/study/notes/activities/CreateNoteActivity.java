c2d2d55eb8d6e892ced378363687092dd333014a
kjy <aty753@naver.com>
2020-11-23 22:06:27 +0900

    image path 관련 함수 작성

--- app/src/main/java/com/study/notes/activities/CreateNoteActivity.java
+++ app/src/main/java/com/study/notes/activities/CreateNoteActivity.java
@@ -10,6 +10,7 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -43,6 +44,7 @@ public class CreateNoteActivity extends AppCompatActivity {
     private ImageView imageNote;
 
     private String selectedNoteColor;
+    private String selectedImagePath;
 
     private static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
     private static final int REQUEST_CODE_SELECT_IMAGE = 2;
@@ -82,6 +84,7 @@ public class CreateNoteActivity extends AppCompatActivity {
         });
 
     selectedNoteColor = "#333333";  /*Default note color => colorSearchBackground*/
+    selectedImagePath = "";
 
         initMiscellaneous();
         setSubtitleIndicatorColor();
@@ -105,6 +108,7 @@ public class CreateNoteActivity extends AppCompatActivity {
         note.setNoteText(inputNoteText.getText().toString());
         note.setDateTime(textDateTime.getText().toString());
         note.setColor(selectedNoteColor);
+        note.setImagePath(selectedImagePath);
 
 
         @SuppressLint("StaticFieldLeak")
@@ -276,12 +280,30 @@ public class CreateNoteActivity extends AppCompatActivity {
                     imageNote.setImageBitmap(bitmap);
                     imageNote.setVisibility(View.VISIBLE);
 
+                    selectedImagePath = getPathFromUri(selectedIamgeUri);
+
                 } catch(Exception exception){
                     Toast.makeText(this,exception.getMessage(),Toast.LENGTH_SHORT).show();
                 }
             }
             }
         }
+
+    }
+
+    private String getPathFromUri(Uri contentUri){
+        String filePath;
+        Cursor cursor = getContentResolver()
+                .query(contentUri, null, null,null,null);
+        if(cursor == null){
+            filePath = contentUri.getPath();
+        } else {
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndex("_data");
+            filePath = cursor.getString(index);
+            cursor.close();
+        }
+        return filePath;
 
     }
 }
