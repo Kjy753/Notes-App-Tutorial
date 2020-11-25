0fcc6b3a4f3b7834ac80c043601b3ad81bac5be8
kjy <aty753@naver.com>
2020-11-25 22:36:28 +0900

    getNote() 메소드에 requestCode 를 이용한 add,show,update 작업 추가

--- app/src/main/java/com/study/notes/activities/MainActivity.java
+++ app/src/main/java/com/study/notes/activities/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.study.notes.R;
 import com.study.notes.adapters.NotesAdapter;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements NotesListener {
 
     public static final int REQUEST_CODE_ADD_NOTE = 1;
     public static final int REQUEST_CODE_UPDATE_NOTE = 2;   /*메모를 업데이트 하는데 사용 되는 REQUEST_CODE*/
+    public static final int REQUEST_CODE_SHOW_NOTES = 3;    /*모든 코드를 보여줄떄 사용하는 코드*/
 
     private RecyclerView notesRecyclerView;
     private List<Note> noteList;
@@ -60,7 +62,11 @@ public class MainActivity extends AppCompatActivity implements NotesListener {
         notesRecyclerView.setAdapter(notesAdapter);
 
 
-        getNotes();
+        getNotes(REQUEST_CODE_SHOW_NOTES);
+        /*getNotes 메소드는 onCreate 메소드에서 호출이 됩니다.
+        * 즉, 응용 프로그램이 방금 시작 되었고 DB의 모든 메모를 표시해야 하며
+        * 이것이 REQUEST_CODE_SHOW_NOTES 를 해당 메소드에 전달 해주어야
+        * 전체 메모가 한번에 나옵니다.*/
     }
 
     @Override
@@ -73,8 +79,8 @@ public class MainActivity extends AppCompatActivity implements NotesListener {
     }
 
     // 데이터 베이스에서 노트를 가져오는 데도 비동기 작업이 필요합니다.
-    private void getNotes(){
-
+    private void getNotes(final int requestcode){
+            /*요청 코드를 getNotes 매개변수로 넣기.*/
         @SuppressLint("StaticFieldLeak")
         class GetNoteTask extends AsyncTask<Void, Void, List<Note>>{
             @Override
@@ -113,7 +119,11 @@ public class MainActivity extends AppCompatActivity implements NotesListener {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK){
-            getNotes();
+            getNotes(REQUEST_CODE_ADD_NOTE);
+        } else if(requestCode == REQUEST_CODE_UPDATE_NOTE && resultCode == RESULT_OK){
+            if(data != null){
+                getNotes(REQUEST_CODE_UPDATE_NOTE);
+            }
         }
     }
 }
